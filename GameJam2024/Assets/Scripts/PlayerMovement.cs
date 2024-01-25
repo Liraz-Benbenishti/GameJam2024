b96ed2ab45a501b7e4b812a4b9cda0409fc75202
Liraz-Benbenishti <lirazben6@gmail.com>
2024-01-25 21:29:30 +0200

    Add Player right, left and jump movements.

--- GameJam2024/Assets/Scripts/PlayerMovement.cs
+++ GameJam2024/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float velocity = 5f;
+    public float jumpForce = 10f;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        // Get input from the player
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Calculate movement vector
+        Vector2 movement = new Vector2(horizontalInput, 0f);
+
+        // Apply movement to the Rigidbody2D
+        rb.velocity = new Vector2(movement.x * velocity, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
