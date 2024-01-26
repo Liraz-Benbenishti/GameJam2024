3c0904ddb376adfe7b0d66ce2995316b616ae0ea
Amitai Mazliah <amitaimazliah@gmail.com>
2024-01-26 18:12:48 +0200

    powerups

---
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public abstract class PowerUp : MonoBehaviour
+{
+    public float duration;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag == "Player")
+        {
+            var player = other.gameObject.GetComponent<PlayerMovement>();
+            
+            player.ApplyPowerUp(this);
+            StartCoroutine(player.RemovePowerUp(this));
+        }
+    }
+}
