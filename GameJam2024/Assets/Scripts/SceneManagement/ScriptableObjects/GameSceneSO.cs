acf87daed84cbb125a5ddb8a81d3a4882ce49493
Amitai Mazliah <amitaimazliah@gmail.com>
2024-01-26 13:01:28 +0200

    init

---
+++ GameJam2024/Assets/Scripts/SceneManagement/ScriptableObjects/GameSceneSO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+/// <summary>
+/// This class is a base class which contains what is common to all game scenes (Locations or Menus)
+/// </summary>
+public class GameSceneSO : ScriptableObject
+{
+    [Header("Information")] public AssetReference sceneReference;
+
+    [Header("Sounds")] public AudioCueSO musicTrack;
+}
